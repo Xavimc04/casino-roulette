b08aa6abdfb71ee3fc97654562c786fba1b16084
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-01-02 23:14:17 +0100

    update(middleware): Prevent auth from casino route prefix

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,19 @@ import { NextRequest, NextResponse } from "next/server";
 import { isAuthenticated } from "@/services/auth/api.service";
 
 export default async function middleware(req: NextRequest) {
+    const isAllowed = await isAuthenticated(req);
+    
     if(req.nextUrl.pathname.startsWith('/auth')) {
-        const isAllowed = await isAuthenticated(req);
-
         if(!isAllowed) { 
             return NextResponse.next();
         }
         
         return NextResponse.redirect(new URL('/', req.url).toString());
+    } else if(req.nextUrl.pathname.startsWith('/casino')) {
+        if(isAllowed) { 
+            return NextResponse.next();
+        }
+        
+        return NextResponse.redirect(new URL('/auth/login', req.url).toString());
     }
 }
